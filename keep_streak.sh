72713201c400b599618a612215db62599a6d0548
glyodev <admin@glyo.dev>
2025-12-23 22:57:28 -0400

    update main

--- keep_streak.sh
+++ keep_streak.sh
@@ -5,6 +5,6 @@ cd /opt/glyodev || exit 1
 echo "$(date)" | sudo tee -a .keep_streak > /dev/null
 sudo git add .keep_streak
 sudo git commit -m "auto: keep streak"
-sudo git push origin keep
+sudo git push origin main
 
 echo "Git push glyodev: $(date)"
